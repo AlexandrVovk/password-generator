5733b067fc4f7022c869925c05154ce121edeede
Alexandr Vovk <Alexandr.Vovk.V@gmail.com>
2023-05-29 22:29:25 +0200

    Update password.py

--- password.py
+++ password.py
@@ -1,30 +1,18 @@
 import secrets
 import string
 
-letters = string.ascii_letters
-digits = string.digits
-special_chars = string.punctuation
-password_length = 15
 
-def main() -> str:
-    letters = string.ascii_letters
-    digits = string.digits
-    special_chars = string.punctuation
-    password_length = 15
+def main(password_length = 15) -> str:
+    alphabet = string.ascii_letters + string.digits + string.punctuation
 
     dubious_special_chars = ['!', '|', '\\', '/',  '`', "'", ';', ':' ,',', '.']
-    for item in dubious_special_chars:
-        special_chars = special_chars.replace(item, '')
-
     dubious_letters = ['l', 'i', 'I', 'O', 'o']
-    for item in dubious_letters:
-        letters = letters.replace(item, '')
-
     dubious_digits = ['1', '0']
-    for item in dubious_digits:
-        digits = digits.replace(item, '')
+    dubious = dubious_special_chars + dubious_letters + dubious_digits
+    
+    for item in dubious:
+        alphabet = alphabet.replace(item, '')
 
-    alphabet = letters + special_chars + digits
     password = ''.join(secrets.choice(alphabet) for i in range(password_length))
     return password
 
